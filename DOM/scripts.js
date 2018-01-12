286e894e9f0aa36bf91c3792634d2f5e94fdb92b
Matthew Aderhold <matthewaderhold@Matthews-MacBook-Pro.local>
2018-01-12 14:33:53 -0600

    Created a paragraph that changes random colors

--- DOM/scripts.js
+++ DOM/scripts.js
@@ -1,3 +1,4 @@
+// Create button that creates an alert with a nice message
 let btn = document.createElement("button");
 let btnText = document.createTextNode("Great Job! This Button is Here!");
 btn.appendChild(btnText);
@@ -7,6 +8,7 @@ btn.addEventListener("click", function() {
   alert("Great Job! The Listener Works!");
 });
 
+// Create alert with text equal to the value input by user
 let btnTwo = document.getElementById("btn2");
 
 btnTwo.addEventListener("click", function() {
@@ -14,14 +16,29 @@ btnTwo.addEventListener("click", function() {
   alert(textBoxValue);
 });
 
-
 // Change div color on hover, change back when not over
 let divHover = document.getElementById("divHover");
 
 divHover.addEventListener("mouseover", function() {
-    divHover.style.backgroundColor = "yellow";
+  divHover.style.backgroundColor = "yellow";
 });
 
 divHover.addEventListener("mouseout", function() {
-    divHover.style.backgroundColor = "black";
+  divHover.style.backgroundColor = "black";
+});
+
+// Make a paragraph change random colors when clicked
+let randomColorPara = document.getElementById("RandomColorP");
+
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+randomColorPara.addEventListener("click", function() {
+  randomColorPara.style.color = getRandomColor();
 });
