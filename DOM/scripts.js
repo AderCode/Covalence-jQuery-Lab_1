3c910c1a625bc7d1743d08e684020466f7ff0323
Matthew Aderhold <matthewaderhold@Matthews-MacBook-Pro.local>
2018-01-12 12:42:18 -0600

    Added EventListener to Button that creates an alert.

--- DOM/scripts.js
+++ DOM/scripts.js
@@ -1,4 +1,9 @@
 let button = document.createElement('button');
-let btnText = document.createTextNode('Great Job! It Works!');
+let btnText = document.createTextNode('Great Job! The Button is Here!');
 button.appendChild(btnText);
 document.body.appendChild(button);
+
+button.addEventListener("click", function() {
+    alert("Great Job! The Listener Works!");
+});
+
